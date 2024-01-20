c1b4e16f482bc80032d51c2832989f9ae06777ba
Sunny Subash <Sunny_subash@gmail.com>
2024-01-20 14:41:40 +0000

    done with get api

--- server.js
+++ server.js
@@ -1,5 +1,61 @@
 const express = require("express");
 const app = express();
+const {MongoClient} = require("mongodb");
+
+const pass = "Facebook123--123";
+const uri = `mongodb+srv://sunnysubash:${pass}@cluster.cxcal2p.mongodb.net/?retryWrites=true&w=majority`;
+const db_name = "school_classes_db";
+const db_lesson_collection_name = "lessons";
+const db_order_collection_name = "orders";
+
+
+const client = new MongoClient(uri);
+
+const fetchLessons = async (req, res) => {
+
+    try {
+        await client.connect(); 
+
+    const db = client.db(db_name);
+
+    const lessons_collection = db.collection(db_lesson_collection_name);
+
+    const fetchedLessons = await lessons_collection.find({}).toArray();
+
+    res.json(fetchedLessons);
+
+    } catch(error){
+        res.status(500).json({message: error.message});
+    }
+    
+}
+
+const fetchOrders = async (req, res) => {
+
+    try {
+        await client.connect(); 
+
+    const db = client.db(db_name);
+
+    const orders_collection = db.collection(db_order_collection_name);
+
+    const fetchedOrders = await orders_collection.find({}).toArray();
+
+    res.json(fetchedOrders);
+
+    } catch(error){
+        res.status(500).json({message: error.message});
+    }
+    
+}
+
+
+
+
+
+
+app.get("/lessons", fetchLessons);
+app.get("/orders", fetchOrders);
 
 
 const port = process.env.PORT || 3000;
